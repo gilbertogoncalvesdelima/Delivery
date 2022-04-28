5068bb8efe8087f750626f82214b59a1d42a10eb
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2022-04-28 13:53:20 -0300

    Container

---
+++ src/AppStyled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  background-color: #ff0000;
+`;
+
+export const Menu = styled.div``;
+
+export const PageBody = styled.div``;
