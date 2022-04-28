6b9f24681f172853b30aa95879d05b36c26cd18c
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2022-04-28 13:24:17 -0300

    teste

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
-import { Container, Titulo } from './styled';
+import { Container, Titulo } from "./styled";
 
 export default () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleButtonClick = () => {
-        history.push('/tela2/testador');
-    }
+  const handleButtonClick = () => {
+    history.push("/tela2/testador");
+  };
 
-    return (
-        <Container>
-            <Titulo>Homepage</Titulo>
-            <button onClick={handleButtonClick}>Ir para Tela 2</button>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Titulo>Homepage teste</Titulo>
+      <button onClick={handleButtonClick}>Ir para Tela 2</button>
+    </Container>
+  );
+};
