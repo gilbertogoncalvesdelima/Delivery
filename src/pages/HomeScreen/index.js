182661f5e7b644a8c26681bcaa07623ff23c39ce
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2022-05-10 15:59:54 -0300

    home (Parte 1)

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { Container } from "./styled";
 
+import Header from "../../components/Header";
+
 export default () => {
   const history = useHistory();
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 };
