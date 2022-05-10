2acc4d2d16232220386440bdad3210a9439f3afe
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2022-05-10 14:33:28 -0300

    extrutura (parte 4)

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -4,10 +4,12 @@ import { CartArea, CartHeader, CartIcon, CartText, CartBody } from "./styled";
 export default () => {
   return (
     <CartArea>
+      {/*Cabeça do meu carrinho*/}
       <CartHeader>
         <CartIcon src="/assets/cart.png" />
         <CartText>Meu Carrinho (x)</CartText>
       </CartHeader>
+      {/*Corpo do meu carrinho*/}
       <CartBody></CartBody>
     </CartArea>
   );
